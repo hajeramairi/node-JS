c5daac3c3f05b39f421d38fe579a8b3e1af9dbb0
hajeramairi <47661554+hajeramairi@users.noreply.github.com>
2024-03-01 21:52:51 +0100

    bbb

--- bookstore/frontend/src/App.jsx
+++ bookstore/frontend/src/App.jsx
@@ -7,6 +7,27 @@ import EditBook from './pages/EditBook';
 import DeleteBook from './pages/DeleteBook';
 
 const App = () => {
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const bookData = {
+      title,
+      author,
+      publishYear: Number(publishYear), // Ensure publishYear is sent as a Number
+    };
+
+    axios.post('https://back-seven-gilt.vercel.app/books', bookData)
+      .then(response => {
+        console.log('Book Created:', response.data);
+        // Optionally reset form fields or redirect the user
+      })
+      .catch(error => console.error('There was an error creating the book:', error));
+  };
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
