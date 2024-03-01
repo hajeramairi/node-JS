0fc110aaf39e056c4713529b9759745d3b3cbbbf
hajeramairi <47661554+hajeramairi@users.noreply.github.com>
2024-03-01 22:26:52 +0100

    Update ShowBook.jsx

--- bookstore/frontend/src/pages/ShowBook.jsx
+++ bookstore/frontend/src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`https://back-ten-zeta.vercel.app/books/${id}`)
+      .get(`https://back-ten-zeta.vercel.app/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
